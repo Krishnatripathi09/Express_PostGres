a17f76b595385a351be8f1fd9f37b2fe359e8b2a
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-03-22 18:18:11 +0530

    feat(user):Implemented pagination to limit the number of users while making get request

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 const express = require("express");
-
+const authRouter = require("./routes/auth");
 const app = express();
-const { pool } = require("./dbConnection");
+
+app.use(express.json());
+
+const { connectDB } = require("./config/dbConnection");
+
 const PORT = 3000;
 
-pool
-  .connect()
-  .then(() => {
-    console.log("Connected to the database SuccessFully");
-    app.listen(PORT, () => {
-      console.log(`Server is Running on ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
+app.use("/", authRouter);
+connectDB().then(() => {
+  console.log("Connected to DataBase Successfully");
+
+  app.listen(PORT, () => {
+    console.log(`Server is Running on ${PORT}`);
   });
-
-app.post("/signup", (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-
-  const query = `Insert INTO USERS (firstName,lastName,email,password) VALUES {$1, $2, $3, $4} RETURNING *`;
-
-  
 });
---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const authRouter = express.Router();
+const User = require("../model/userSchema");
+
+authRouter.post("/signup", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  const data = new User({
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+
+  await data.save();
+
+  res.status(201).send("User Created Successfully");
+});
+
+module.exports = authRouter;
+
+authRouter.get("/user", async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  let limit = parseInt(req.query.limit) || 1;
+  limit = limit > 50 ? 50 : limit;
+  const skip = (page - 1) * 10;
+
+  const user = await User.find({})
+    .select("firstName lastName email")
+    .skip()
+    .limit(limit);
+
+  res.status(200).send("found User =>" + user);
+});
